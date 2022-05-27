741cd8b94f8156f3b1702315ba9fedea0a20bcc0
heyimandrew <imandrew@mailbox.org>
2022-05-27 01:51:36 -0400

    made game board

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import random
 
 boardSpaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+boardRows = 3
+boardColumns = 3
+
+
+def print_board():
+    for row in range(boardRows):
+        print("\n+---+---+---+")
+        print("|", end="")
+        for col in range(boardColumns):
+            print("", gameBoard[row][col], end=" |")
+    print("\n+---+---+---+")
+
+print_board()
